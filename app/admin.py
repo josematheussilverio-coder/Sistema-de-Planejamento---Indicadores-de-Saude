b7bbd8abea4c7835a5cfd4b3787c9daf582b6f28
josematheussilverio-coder <josematheussilverio@gmail.com>
2026-04-07 12:51:19 -0300

    Add files via upload

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import GrupoRisco, Cidadao
 
-# Register your models here.
+admin.site.register(GrupoRisco)
+
+
+@admin.register(Cidadao)
+class CidadaoAdmin(admin.ModelAdmin):
+    list_display = ('nome','cns', 'microarea', 'bairro', 'idade')
+
+    search_fields = ('nome', 'cns', 'cpf')
+
+    list_filter = ('microarea', 'grupos_de_risco')
